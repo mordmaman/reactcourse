ee4b46e5fc028746c5cbae75de7eff435a9bf8f0
Mord Maman <jaime.mord.maman@gmail.com>
2017-06-26 15:52:08 +0100

    creates first component, Greeter

--- HelloReact/public/app.jsx
+++ HelloReact/public/app.jsx
@@ -1,4 +1,15 @@
-ReactDOM.render(
-            <h1>Hello React!</h1>,
-            document.getElementById('app')
+var Greeter = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Hello React!</h1>
+            </div>
         );
+    }
+});
+
+
+ReactDOM.render(
+    <Greeter/>,
+    document.getElementById('app')
+);
